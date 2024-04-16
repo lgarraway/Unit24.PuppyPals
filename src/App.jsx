987c9b6023bd4261bf7df93dd44fd1f010999ef6
Londell <londellg@icloud.com>
2024-04-16 16:03:37 -0500

    Finished & Added CSS.

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,32 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+
   const [puppies, setPuppies] = useState(puppyList)
   console.log(puppies)
+
+  const [featPupId, setFeatPupId] = useState(null)
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
 
   return (
     <>
       <div>
         {
           puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
+            return <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
           })
         }
+        { featPupId && (
+        <div className='PupInfo'>
+          <h2 className='PupName'>{featuredPup.name}</h2>
+          <ul className='PupInfoList'>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       </div>
     </>
   )
